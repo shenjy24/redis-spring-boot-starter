d43cdb1174e1236a8aaf67aafe57e05a84aefbab
shenjy <shenjiayun@hzyushi.cn>
2019-03-12 15:52:00 +0800

    分布式锁

--- src/main/java/com/jonas/redis/service/lock/RedisDistributedLock.java
+++ src/main/java/com/jonas/redis/service/lock/RedisDistributedLock.java
@@ -2,18 +2,14 @@ package com.jonas.redis.service.lock;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.dao.DataAccessException;
-import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.RedisStringCommands;
+import org.springframework.data.redis.connection.ReturnType;
 import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.types.Expiration;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
-import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisCluster;
-import redis.clients.jedis.JedisCommands;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.nio.charset.Charset;
 import java.util.UUID;
 
 /**
@@ -28,7 +24,7 @@ public class RedisDistributedLock extends AbstractDistributedLock {
 
     private RedisTemplate<Object, Object> redisTemplate;
 
-    private ThreadLocal<String> lockFlag = new ThreadLocal<String>();
+    private ThreadLocal<String> lockFlag = new ThreadLocal<>();
 
     public static final String UNLOCK_LUA;
 
@@ -66,13 +62,12 @@ public class RedisDistributedLock extends AbstractDistributedLock {
 
     private boolean setRedis(String key, Long expire) {
         try {
-            String result = redisTemplate.execute((RedisCallback<String>) connection -> {
-                JedisCommands commands = (JedisCommands) connection.getNativeConnection();
+            Boolean result = redisTemplate.execute((RedisCallback<Boolean>) connection -> {
                 String uuid = UUID.randomUUID().toString();
                 lockFlag.set(uuid);
-                return commands.set(key, uuid, "NX", "PX", expire);
+                return connection.set(key.getBytes(Charset.forName("UTF-8")), uuid.getBytes(Charset.forName("UTF-8")), Expiration.milliseconds(expire), RedisStringCommands.SetOption.SET_IF_ABSENT);
             });
-            return !StringUtils.isEmpty(result);
+            return result;
         } catch (Exception e) {
             logger.error("set redis occurred an exception", e);
         }
@@ -81,33 +76,13 @@ public class RedisDistributedLock extends AbstractDistributedLock {
 
     @Override
     public boolean releaseLock(String key) {
-        // 释放锁的时候，有可能因为持锁之后方法执行时间大于锁的有效期，此时有可能已经被另外一个线程持有锁，所以不能直接删除
         try {
-            List<String> keys = new ArrayList<String>();
-            keys.add(key);
-            List<String> args = new ArrayList<String>();
-            args.add(lockFlag.get());
-
+            // 释放锁的时候，有可能因为持锁之后方法执行时间大于锁的有效期，此时有可能已经被另外一个线程持有锁，所以不能直接删除
             // 使用lua脚本删除redis中匹配value的key，可以避免由于方法执行时间过长而redis锁自动过期失效的时候误删其他线程的锁
-            // spring自带的执行脚本方法中，集群模式直接抛出不支持执行脚本的异常，所以只能拿到原redis的connection来执行脚本
-            Long result = redisTemplate.execute(new RedisCallback<Long>() {
-                @Override
-                public Long doInRedis(RedisConnection connection) throws DataAccessException {
-                    Object nativeConnection = connection.getNativeConnection();
-                    // 集群模式和单机模式虽然执行脚本的方法一样，但是没有共同的接口，所以只能分开执行
-                    // 集群模式
-                    if (nativeConnection instanceof JedisCluster) {
-                        return (Long) ((JedisCluster) nativeConnection).eval(UNLOCK_LUA, keys, args);
-                    }
+            RedisCallback<Boolean> callback =
+                    connection -> connection.eval(UNLOCK_LUA.getBytes(), ReturnType.BOOLEAN, 1, key.getBytes(Charset.forName("UTF-8")), lockFlag.get().getBytes(Charset.forName("UTF-8")));
 
-                    // 单机模式
-                    else if (nativeConnection instanceof Jedis) {
-                        return (Long) ((Jedis) nativeConnection).eval(UNLOCK_LUA, keys, args);
-                    }
-                    return 0L;
-                }
-            });
-            return result != null && result > 0;
+            return redisTemplate.execute(callback);
         } catch (Exception e) {
             logger.error("release lock occurred an exception", e);
         }
